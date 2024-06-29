7526d9ce8a57cf47c4b7ed4920ba2c4dcda071d0
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-06-29 03:39:28 -0300

    Time limit exceed on bee 1023 :(

--- bee1023.py
+++ bee1023.py
@@ -2,9 +2,17 @@
 
 import math
 
+city_id = 1
+
 while True:
-    city_id = 1
     houses = int(input(''))
+
+    if not houses:
+        break
+
+    if city_id > 1:
+        print("")
+
     city_info: list[dict[int, int]] = []
     total_consum, total_residents = 0, 0
     for i in range(houses):
@@ -13,6 +21,9 @@ while True:
         total_residents += residents
         city_info.append((residents, int(house_consum/residents)))
     city_info.sort(key=lambda x: x[1])
-    print(city_info)
-    print(f'Consumo medio: {math.floor(total_consum/total_residents * 10 ** 2) / 10 ** 2} m3.')
 
+    # Output
+    print(f"Cidade# {city_id}:")
+    print(" ".join(str(x).replace(", ", "-").replace("(", "").replace(")", "") for x in city_info))
+    print(f'Consumo medio: {(math.floor(total_consum/total_residents * 10 ** 2) / 10 ** 2):.2f} m3.')
+    city_id += 1
---
+++ bee2312.py
@@ -0,0 +1,9 @@
+"""Beecrowd | 2312"""
+
+
+class Country:
+    """Crountry infos"""
+    def __init__(self, *args):
+        self.name, self.gold, self.silver, self.bronze = args
+
+
