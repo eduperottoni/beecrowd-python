40ad16e1374428d4d76b081d6af4e529906e1671
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-07-06 00:50:23 -0300

    bee1015

---
+++ bee1015.py
@@ -0,0 +1,8 @@
+"""Beecrowd | 1015"""
+
+xa, ya = [float(x) for x in input().split()]
+xb, yb = [float(x) for x in input().split()]
+
+distance = ((xb-xa)**2 + (yb-ya)**2)**(1/2)
+
+print(f'{distance:.4f}')
