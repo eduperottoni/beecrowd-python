233e6944098f1bc4a984da7295136d5407213fb5
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-07-29 20:34:16 -0300

    bee1870

--- bee1870.py
+++ bee1870.py
@@ -5,7 +5,6 @@ def search_fan(direction: str, index: int, line: int) -> int:
     fan_index = index
     if direction == 'left':
         for elem in reversed(line[:index + 1]):
-            print('ops')
             if elem != 0:
                 return fan_index
             fan_index -= 1
@@ -17,25 +16,24 @@ def search_fan(direction: str, index: int, line: int) -> int:
 
 
 while True:
-lines, columns, start_position = (int(x) for x in input().split())
-start_position -= 1
+    lines, columns, start_position = (int(x) for x in input().split())
+    if lines == columns == start_position == 0:
+        break
 
-matrix = []
+    start_position -= 1
+    final_status = ""
 
-for line in range(lines):
-    l = [int(x) for x in input().split()]
-    left = search_fan('left', start_position, l)
-    right = search_fan('r', start_position, l)
+    for line in range(lines):
+        l = [int(x) for x in input().split()]
+        left = search_fan('left', start_position, l)
+        right = search_fan('r', start_position, l)
 
-    if left == start_position or right == start_position:
-        print(f'BOOM {line + 1} {start_position + 1}')
+        if left == start_position or right == start_position:
+            final_status = f'BOOM {line + 1} {start_position + 1}'
 
-    start_position += (l[left] - l[right])
-    if l[start_position] != 0:
-        print(f'BOOM {line + 1} {start_position + 1}')
+        start_position += (l[left] - l[right])
+        if l[start_position] != 0:
+            final_status = f'BOOM {line + 1} {start_position + 1}'
 
-    print(f'vamos para {start_position}')
-
-    matrix.append(l)
-
-
+    out = f'OUT {start_position + 1}'
+    print(f'{final_status if final_status else out}')
