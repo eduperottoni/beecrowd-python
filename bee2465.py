4a86b87e41637822393db1e065243ddb008a29bc
Eduardo Perottoni <edu.perottoni@gmail.com>
2024-08-01 19:54:21 -0300

    bee2465

--- bee2465.py
+++ bee2465.py
@@ -1,6 +1,6 @@
 """Beecrowd | 2465"""
 
-def search_next_step(matrix: list[list[int]], position: tuple[int], visited: list[tuple[int]]) -> tuple[int]:
+def search_next_step(matrix: list[list[int]], position: tuple[int], visited: list[tuple[int]], to_visit: list[tuple[int]]) -> None:
     top = (position[0] - 1, position[1]) if position[0] - 1 > -1 else None
     left = (position[0], position[1] - 1) if position[1] - 1 > -1 else None
     bottom = (position[0] + 1, position[1]) if position[0] + 1 < len(matrix) else None
@@ -9,10 +9,10 @@ def search_next_step(matrix: list[list[int]], position: tuple[int], visited: lis
     iteration = [i for i in (top, left, bottom, right) if i is not None]
 
     for (i, j) in iteration:
-        if (i, j) not in visited and matrix[i][j]:
-            return (i, j)
+        if (i, j) not in visited and (i, j) not in to_visit and matrix[i][j] >= matrix[position[0]][position[1]]:
+            to_visit.append((i, j))
 
-    return (None, None)
+    visited.append(position)
 
 
 n = int(input())
@@ -23,11 +23,12 @@ matrix = []
 for i in range(n):
     matrix.append([int(x) for x in input().split()])
 
-flags_up = 0
 initial_position = (x_coord, y_coord)
+to_visit = [initial_position]
+visited = []
 
-# Cada nodo deve ter a referência de quem o passou e para onde está apontando
+while len(to_visit) > 0:
+    position = to_visit.pop(0)
+    search_next_step(matrix, position, visited, to_visit)
 
-# Cada aluno dever executar esse loop 
-for i in ['r', 'b', 'l', 't']:
-    
+print(len(visited))
